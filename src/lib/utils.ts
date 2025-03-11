8ba2b985a9f4e91e35ccb3d31e4e25da4044a715
krieger_ <julian@wagnerjw.dev>
2025-03-10 20:57:49 -0700

    cloned project

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,10 @@
+
+import { clsx, type ClassValue } from "clsx";
 import { redirect } from 'next/navigation';
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 /**
