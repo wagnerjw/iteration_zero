09e3bd07beed70c8984142bdbc84bb958dd2559e
krieger_ <julian@wagnerjw.dev>
2025-04-24 13:00:21 -0700

    feat: fix metadata in layouts

--- src/app/(private)/[username]/layout.tsx
+++ src/app/(private)/[username]/layout.tsx
@@ -1,14 +1,16 @@
 import '@/app/globals.css';
 import HeaderAuth from '@/components/prebuilt/header-auth';
 import { jbmono } from '@/fonts/JetbrainsMono';
+import type { Metadata } from 'next';
+
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : 'http://localhost:3000';
 
-export const metadata = {
+export const metadata: Metadata = {   
   metadataBase: new URL(defaultUrl),
-  title: 'The Ultimate AI Starter Template',
+  title: 'AI Starter Template',
   description: 'All The Things You Need To Get Started With AI',
 };
 
